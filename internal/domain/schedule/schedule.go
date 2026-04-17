21847ea497c93cb66075c4e616a9d1cc120e07a6
leyl1ne <d-khodin@inbox.ru>
2026-04-17 14:22:59 +0300

    feat(schedule): add ShouldRunToday and NormalizeDate functions

--- internal/domain/schedule/schedule.go
+++ internal/domain/schedule/schedule.go
@@ -47,3 +47,59 @@ func (s *Schedule) Validate() error {
 
 	return nil
 }
+
+func (s *Schedule) ShouldRunToday(today time.Time) bool {
+	// до старта — не запускаем
+	if today.Before(NormalizeDate(s.StartDate)) {
+		return false
+	}
+
+	// после конца — не запускаем
+	if s.EndDate != nil && today.After(NormalizeDate(*s.EndDate)) {
+		return false
+	}
+
+	switch s.Type {
+
+	case Daily:
+		if s.Interval == nil {
+			return false
+		}
+
+		days := int(today.Sub(NormalizeDate(s.StartDate)).Hours() / 24)
+		return days%*s.Interval == 0
+
+	case Monthly:
+		for _, d := range s.DaysOfMonth {
+			if d == today.Day() {
+				return true
+			}
+		}
+		return false
+
+	case Specific:
+		for _, d := range s.SpecificDates {
+			if NormalizeDate(d).Equal(today) {
+				return true
+			}
+		}
+		return false
+
+	case EvenOdd:
+		if s.EvenOdd == nil {
+			return false
+		}
+
+		if *s.EvenOdd == "even" {
+			return today.Day()%2 == 0
+		}
+		return today.Day()%2 != 0
+	}
+
+	return false
+}
+
+func NormalizeDate(t time.Time) time.Time {
+	y, m, d := t.Date()
+	return time.Date(y, m, d, 0, 0, 0, 0, time.UTC)
+}
