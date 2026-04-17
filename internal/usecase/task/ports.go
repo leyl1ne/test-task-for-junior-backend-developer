362bbb02199bb697612f697d3d3a94ba0fd7435a
leyl1ne <d-khodin@inbox.ru>
2026-04-17 14:28:48 +0300

    feat(task): add schedule input and map

--- internal/usecase/task/ports.go
+++ internal/usecase/task/ports.go
@@ -2,16 +2,22 @@ package task
 
 import (
 	"context"
+	"time"
 
+	scheduledomain "example.com/taskservice/internal/domain/schedule"
 	taskdomain "example.com/taskservice/internal/domain/task"
 )
 
-type Repository interface {
+type TaskRepository interface {
 	Create(ctx context.Context, task *taskdomain.Task) (*taskdomain.Task, error)
 	GetByID(ctx context.Context, id int64) (*taskdomain.Task, error)
 	Update(ctx context.Context, task *taskdomain.Task) (*taskdomain.Task, error)
 	Delete(ctx context.Context, id int64) error
 	List(ctx context.Context) ([]taskdomain.Task, error)
+}
+
+type ScheduleRepository interface {
+	Create(ctx context.Context, s *scheduledomain.Schedule) (*scheduledomain.Schedule, error)
 }
 
 type Usecase interface {
@@ -26,10 +32,39 @@ type CreateInput struct {
 	Title       string
 	Description string
 	Status      taskdomain.Status
+	Date        *time.Time
+	Schedule    *ScheduleInput
 }
 
 type UpdateInput struct {
 	Title       string
 	Description string
 	Status      taskdomain.Status
+}
+
+type ScheduleInput struct {
+	Type string
+
+	Interval      *int
+	DaysOfMonth   []int
+	SpecificDates []time.Time
+	EvenOdd       *string
+
+	StartDate time.Time
+	EndDate   *time.Time
+}
+
+func mapToDomainSchedule(input *ScheduleInput, title string, now time.Time) *scheduledomain.Schedule {
+	return &scheduledomain.Schedule{
+		Title:         title,
+		Type:          scheduledomain.Type(input.Type),
+		Interval:      input.Interval,
+		DaysOfMonth:   input.DaysOfMonth,
+		SpecificDates: input.SpecificDates,
+		EvenOdd:       input.EvenOdd,
+		StartDate:     input.StartDate,
+		EndDate:       input.EndDate,
+		CreatedAt:     now,
+		UpdatedAt:     now,
+	}
 }
